f60a55be037924d00359ee29ad348dc0accd8658
Sahil Khan <meesahil7@gmail.com>
2023-08-25 14:02:16 +0530

    remove lorem ipsum and delete button

--- src/pages/advertisement.tsx
+++ src/pages/advertisement.tsx
@@ -17,180 +17,171 @@ import Image from "next/image";
 type AdvertisementProps = {};
 
 const Advertisement: FC<AdvertisementProps> = () => {
-    const server = "http://localhost:4000";
-    const [rowId, setRowId] = useState<string>("");
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    const api = useApi();
-    const store = useStore();
+  const server = "http://localhost:4000";
+  const [rowId, setRowId] = useState<string>("");
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const api = useApi();
+  const store = useStore();
 
-    function openForm() {
-        setIsOpen(true);
+  function openForm() {
+    setIsOpen(true);
+  }
+
+  function closeForm() {
+    setIsOpen(false);
+  }
+  async function fetchAllContacts() {
+    api.startLoading();
+    try {
+      const allAdvertisements = await getAllAdvertisements();
+      store.actions.setAdvertisements(allAdvertisements);
+      api.setSuccess();
+    } catch (error) {
+      const { response } = error as any;
+      console.log(error);
+      api.setError();
+      api.setResponseMessage(response?.data?.message);
+      store.actions.resetAdvertisements();
+    } finally {
+      api.stopLoading();
     }
+  }
 
-    function closeForm() {
-        setIsOpen(false);
-    }
-    async function fetchAllContacts() {
-        api.startLoading();
-        try {
-            const allAdvertisements = await getAllAdvertisements();
-            store.actions.setAdvertisements(allAdvertisements);
-            api.setSuccess();
-        } catch (error) {
-            const { response } = error as any;
-            console.log(error);
-            api.setError();
-            api.setResponseMessage(response?.data?.message);
-            store.actions.resetAdvertisements();
-        } finally {
-            api.stopLoading();
-        }
-    }
+  useEffect(() => {
+    fetchAllContacts();
 
-    useEffect(() => {
-        fetchAllContacts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  const columns: GridColDef[] = useMemo(
+    () => [
+      { field: "_id", headerName: "ID", width: 90 },
+      {
+        field: "date",
+        headerName: "Date",
+        width: 250,
+        renderCell: (params) =>
+          moment(params.row.registered_at).format("DD/MM/YYYY (hh:mm)"),
+      },
+      {
+        field: "title",
+        headerName: "Title",
+        width: 250,
+        editable: true,
+      },
+      {
+        field: "image",
+        headerName: "Image",
+        width: 75,
+        renderCell: (params) => {
+          return (
+            <>
+              <Image
+                src={`${server}/${params.row.image}`}
+                alt="alt"
+                width={256}
+                height={256}
+                className="w-8 h-8 rounded-full object-cover object-center"
+              />
+            </>
+          );
+        },
+      },
+      {
+        field: "status",
+        headerName: "Status",
+        type: "boolean",
+        width: 150,
+        editable: true,
+      },
+      {
+        field: "actions",
+        headerName: "Actions",
+        width: 250,
+        sortable: false,
+        filterable: false,
+        disableColumnMenu: true,
+        renderCell: (params) => {
+          return (
+            <>
+              <UpdateAction
+                {...{
+                  params,
+                  rowId,
+                  setRowId,
+                  action: updateAdvertisementById,
+                }}
+              />
 
-    const columns: GridColDef[] = useMemo(
-        () => [
-            { field: "_id", headerName: "ID", width: 90 },
-            {
-                field: "date",
-                headerName: "Date",
-                width: 250,
-                renderCell: (params) =>
-                    moment(params.row.registered_at).format(
-                        "DD/MM/YYYY (hh:mm)"
-                    ),
+              <DeleteAction
+                {...{
+                  params,
+                  rowId,
+                  setRowId,
+                  action: deleteAdvertisementById,
+                  storeAction: store.actions.deleteAdvertisemenById,
+                }}
+              />
+            </>
+          );
+        },
+      },
+    ],
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [rowId]
+  );
+
+  const rows = store.advertisementManagement.advertisements;
+
+  return (
+    <BasicLayout>
+      {/* add new faq */}
+      <div aria-label="add a faq" className="py-4 mb-4 space-y-4 max-w-2xl">
+        <h2 className="">Add New Advertisement</h2>
+        <p className="heavy-p">
+          Description should be less than or eqal to 55 characters and
+          description2 should be of less than equal to 90 characters
+        </p>
+        {/* add button */}
+        <button
+          onClick={openForm}
+          className="btn text-white bg-slate-800 hover:bg-slate-700"
+        >
+          New Advertisement
+        </button>
+        {/* popup form */}
+        <Dialog open={isOpen} onClose={closeForm}>
+          <div className="bg-slate-800 text-white ">
+            <DialogTitle>
+              <span className="text-white">Add new Advertisement</span>
+            </DialogTitle>
+            <DialogContent className="">
+              <AdvertisementForm handleClose={closeForm} />
+            </DialogContent>
+          </div>
+        </Dialog>
+      </div>
+      {/* advertisement grid */}
+      <Box sx={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          getRowId={(row) => row._id}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
             },
-            {
-                field: "title",
-                headerName: "Title",
-                width: 250,
-                editable: true,
-            },
-            {
-                field: "image",
-                headerName: "Image",
-                width: 75,
-                renderCell: (params) => {
-                    return (
-                        <>
-                            <Image
-                                src={`${server}/${params.row.image}`}
-                                alt="alt"
-                                width={256}
-                                height={256}
-                                className="w-8 h-8 rounded-full object-cover object-center"
-                            />
-                        </>
-                    );
-                },
-            },
-            {
-                field: "status",
-                headerName: "Status",
-                type: "boolean",
-                width: 150,
-                editable: true,
-            },
-            {
-                field: "actions",
-                headerName: "Actions",
-                width: 250,
-                sortable: false,
-                filterable: false,
-                disableColumnMenu: true,
-                renderCell: (params) => {
-                    return (
-                        <>
-                            <UpdateAction
-                                {...{
-                                    params,
-                                    rowId,
-                                    setRowId,
-                                    action: updateAdvertisementById,
-                                }}
-                            />
-
-                            <DeleteAction
-                                {...{
-                                    params,
-                                    rowId,
-                                    setRowId,
-                                    action: deleteAdvertisementById,
-                                    storeAction:
-                                        store.actions.deleteAdvertisemenById,
-                                }}
-                            />
-                        </>
-                    );
-                },
-            },
-        ],
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        [rowId]
-    );
-
-    const rows = store.advertisementManagement.advertisements;
-
-    return (
-        <BasicLayout>
-            {/* add new faq */}
-            <div
-                aria-label="add a faq"
-                className="py-4 mb-4 space-y-4 max-w-2xl"
-            >
-                <h2 className="">Add New Advertisement</h2>
-                <p className="heavy-p">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Mollitia facere, quasi soluta sed quisquam suscipit
-                    necessitatibus impedit.
-                </p>
-                {/* add button */}
-                <button
-                    onClick={openForm}
-                    className="btn text-white bg-slate-800 hover:bg-slate-700"
-                >
-                    New Advertisement
-                </button>
-                {/* popup form */}
-                <Dialog open={isOpen} onClose={closeForm}>
-                    <div className="bg-slate-800 text-white ">
-                        <DialogTitle>
-                            <span className="text-white">
-                                Add new Advertisement
-                            </span>
-                        </DialogTitle>
-                        <DialogContent className="">
-                            <AdvertisementForm handleClose={closeForm} />
-                        </DialogContent>
-                    </div>
-                </Dialog>
-            </div>
-            {/* advertisement grid */}
-            <Box sx={{ height: 400, width: "100%" }}>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    getRowId={(row) => row._id}
-                    initialState={{
-                        pagination: {
-                            paginationModel: {
-                                pageSize: 5,
-                            },
-                        },
-                    }}
-                    pageSizeOptions={[5, 10, 15]}
-                    disableRowSelectionOnClick
-                    onCellEditStop={(params) => setRowId(params.id as string)}
-                />
-            </Box>
-        </BasicLayout>
-    );
+          }}
+          pageSizeOptions={[5, 10, 15]}
+          disableRowSelectionOnClick
+          onCellEditStop={(params) => setRowId(params.id as string)}
+        />
+      </Box>
+    </BasicLayout>
+  );
 };
 
 export default Advertisement;
--- src/pages/subcriptions.tsx
+++ src/pages/subcriptions.tsx
@@ -13,159 +13,151 @@ import { FC, useEffect, useMemo, useState } from "react";
 type SubcriptionsProps = {};
 
 const Subcriptions: FC<SubcriptionsProps> = () => {
-    const [rowId, setRowId] = useState<string>("");
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    const api = useApi();
-    const store = useStore();
+  const [rowId, setRowId] = useState<string>("");
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const api = useApi();
+  const store = useStore();
 
-    function openForm() {
-        setIsOpen(true);
+  function openForm() {
+    setIsOpen(true);
+  }
+
+  function closeForm() {
+    setIsOpen(false);
+  }
+
+  async function fetchAllSubcriptions() {
+    api.startLoading();
+    try {
+      const allSubscriptions = await getAllSubscriptions();
+      store.actions.setSubcriptions(allSubscriptions);
+      api.setSuccess();
+    } catch (error) {
+      const { response } = error as any;
+      console.log(error);
+      api.setError();
+      api.setResponseMessage(response?.data?.message);
+      store.actions.resetSubcriptions();
+    } finally {
+      api.stopLoading();
     }
+  }
 
-    function closeForm() {
-        setIsOpen(false);
-    }
+  useEffect(() => {
+    fetchAllSubcriptions();
 
-    async function fetchAllSubcriptions() {
-        api.startLoading();
-        try {
-            const allSubscriptions = await getAllSubscriptions();
-            store.actions.setSubcriptions(allSubscriptions);
-            api.setSuccess();
-        } catch (error) {
-            const { response } = error as any;
-            console.log(error);
-            api.setError();
-            api.setResponseMessage(response?.data?.message);
-            store.actions.resetSubcriptions();
-        } finally {
-            api.stopLoading();
-        }
-    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  // columns
+  const columns: GridColDef[] = useMemo(
+    () => [
+      { field: "_id", headerName: "ID", width: 90 },
+      {
+        field: "title",
+        headerName: "Title",
+        width: 150,
+      },
+      {
+        field: "validity",
+        headerName: "Validity",
+        width: 110,
+      },
+      {
+        field: "price",
+        headerName: "Price",
+        width: 120,
+      },
+      {
+        field: "compare_price",
+        headerName: "Compare Price",
+        width: 120,
+      },
+      {
+        field: "no_of_reminder",
+        headerName: "No Of Reminder",
+        width: 130,
+      },
+      {
+        field: "status",
+        headerName: "Status",
+        type: "boolean",
+        width: 110,
+        editable: true,
+      },
+      // {
+      //     field: "actions",
+      //     headerName: "Actions",
+      //     width: 250,
+      //     sortable: false,
+      //     filterable: false,
+      //     disableColumnMenu: true,
+      //     renderCell: (params) => {
+      //         return (
+      //             <>
+      //                 <DeleteAction
+      //                     {...{
+      //                         params,
+      //                         rowId,
+      //                         setRowId,
+      //                         action: deleteSubcriptionById,
+      //                         storeAction:
+      //                             store.actions.deleteSubcriptionsById,
+      //                     }}
+      //                 />
+      //             </>
+      //         );
+      //     },
+      // },
+    ],
 
-    useEffect(() => {
-        fetchAllSubcriptions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [rowId]
+  );
 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-    // columns
-    const columns: GridColDef[] = useMemo(
-        () => [
-            { field: "_id", headerName: "ID", width: 90 },
-            {
-                field: "title",
-                headerName: "Title",
-                width: 150,
+  const rows = store.subscrptionManagement.subscriptions;
+  return (
+    <BasicLayout>
+      {/* add new faq */}
+      <div aria-label="add a faq" className="py-4 mb-4 space-y-4 max-w-2xl">
+        <h2 className="">All Subcriptions</h2>
+        {/* add button */}
+        <button
+          onClick={openForm}
+          className="btn text-white bg-slate-800 hover:bg-slate-700"
+        >
+          Add a new Subscription
+        </button>
+        {/* popup form */}
+        <Dialog open={isOpen} onClose={closeForm}>
+          <div className="bg-slate-800 text-white ">
+            <DialogTitle>
+              <h2 className="text-white">Add new Subscription</h2>
+            </DialogTitle>
+            <DialogContent className="">
+              <SubscriptionForm handleClose={closeForm} />
+            </DialogContent>
+          </div>
+        </Dialog>
+      </div>
+      <Box sx={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          getRowId={(row) => row._id}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
             },
-            {
-                field: "validity",
-                headerName: "Validity",
-                width: 110,
-            },
-            {
-                field: "price",
-                headerName: "Price",
-                width: 120,
-            },
-            {
-                field: "compare_price",
-                headerName: "Compare Price",
-                width: 120,
-            },
-            {
-                field: "no_of_reminder",
-                headerName: "No Of Reminder",
-                width: 130,
-            },
-            {
-                field: "status",
-                headerName: "Status",
-                type: "boolean",
-                width: 110,
-                editable: true,
-            },
-            {
-                field: "actions",
-                headerName: "Actions",
-                width: 250,
-                sortable: false,
-                filterable: false,
-                disableColumnMenu: true,
-                renderCell: (params) => {
-                    return (
-                        <>
-                            <DeleteAction
-                                {...{
-                                    params,
-                                    rowId,
-                                    setRowId,
-                                    action: deleteSubcriptionById,
-                                    storeAction:
-                                        store.actions.deleteSubcriptionsById,
-                                }}
-                            />
-                        </>
-                    );
-                },
-            },
-        ],
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        [rowId]
-    );
-
-    const rows = store.subscrptionManagement.subscriptions;
-    return (
-        <BasicLayout>
-            {/* add new faq */}
-            <div
-                aria-label="add a faq"
-                className="py-4 mb-4 space-y-4 max-w-2xl"
-            >
-                <h2 className="">All Subcriptions</h2>
-                <p className="heavy-p">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Mollitia facere, quasi soluta sed quisquam suscipit
-                    necessitatibus impedit.
-                </p>
-                {/* add button */}
-                <button
-                    onClick={openForm}
-                    className="btn text-white bg-slate-800 hover:bg-slate-700"
-                >
-                    Add a new Subscription
-                </button>
-                {/* popup form */}
-                <Dialog open={isOpen} onClose={closeForm}>
-                    <div className="bg-slate-800 text-white ">
-                        <DialogTitle>
-                            <h2 className="text-white">Add new Subscription</h2>
-                        </DialogTitle>
-                        <DialogContent className="">
-                            <SubscriptionForm handleClose={closeForm} />
-                        </DialogContent>
-                    </div>
-                </Dialog>
-            </div>
-            <Box sx={{ height: 400, width: "100%" }}>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    getRowId={(row) => row._id}
-                    initialState={{
-                        pagination: {
-                            paginationModel: {
-                                pageSize: 5,
-                            },
-                        },
-                    }}
-                    pageSizeOptions={[5, 10, 15]}
-                    disableRowSelectionOnClick
-                    onCellEditStop={(params) => setRowId(params.id as string)}
-                />
-            </Box>
-        </BasicLayout>
-    );
+          }}
+          pageSizeOptions={[5, 10, 15]}
+          disableRowSelectionOnClick
+          onCellEditStop={(params) => setRowId(params.id as string)}
+        />
+      </Box>
+    </BasicLayout>
+  );
 };
 
 export default Subcriptions;
